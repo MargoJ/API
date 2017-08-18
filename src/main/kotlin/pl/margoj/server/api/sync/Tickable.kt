44e7fe5a760737712e7f39d53a3088a585342303
KrekBuk <admin@margoj.pl>
2017-08-18 15:53:15 +0200

    Zabieranie wyczerpania, zamiana tickable na taski

--- src/main/kotlin/pl/margoj/server/api/sync/Tickable.kt
+++ src/main/kotlin/pl/margoj/server/api/sync/Tickable.kt
@@ -1,7 +1,13 @@
 package pl.margoj.server.api.sync
 
+/**
+ * Tickable, task wykonywany w glownym watku raz na tick
+ */
 @FunctionalInterface
 interface Tickable
 {
+    /**
+     * Wykonywane w kazdym ticku serwera
+     */
     fun tick(currentTick: Long)
 }
