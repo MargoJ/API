e3791c5bc1ed4902209224af440c6e59351b6701
Inuun <dev.inuun@gmail.com>
2017-08-31 12:58:42 +0200

    Zarządzanie zasobami

--- src/main/kotlin/pl/margoj/server/api/plugin/MargoJPlugin.kt
+++ src/main/kotlin/pl/margoj/server/api/plugin/MargoJPlugin.kt
@@ -1,9 +1,14 @@
 package pl.margoj.server.api.plugin
 
+import com.google.common.io.Files
+import org.apache.commons.io.FileUtils
 import org.apache.logging.log4j.Logger
 import pl.margoj.server.api.Server
 import pl.margoj.server.api.commands.CommandListener
 import pl.margoj.server.api.event.EventListener
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
 
 /**
  * Reprezentuje główną klase pluginu
@@ -58,5 +63,50 @@ abstract class MargoJPlugin<P : MargoJPlugin<P>>
     {
         @Suppress("UNCHECKED_CAST")
         this.server.commandsManager.registerListener(this, constructor(this as P), *commands)
+    }
+
+    /*
+     * InputStream pliku z resources lub null, gdy plik nie istnieje
+     */
+    protected fun getResource(name: String): InputStream?
+    {
+        return Files.asByteSource(File("plugins/${this.annotation.name}/$name")).openStream()
+    }
+
+    /*
+     * Kopiuje plik z resources do wskazanego miejsca, jesli ten nie istnieje
+     */
+    protected fun saveResource(name: String)
+    {
+        try
+        {
+            val file = File("plugins/${this.annotation.name}/$name")
+
+            if (!file.exists())
+            {
+                FileUtils.copyURLToFile(this.javaClass.classLoader.getResource(name), file)
+            }
+        }
+        catch (ex: IOException)
+        {
+            this.logger.error("Błąd podczas zapisywania zasobu '$name'!")
+            ex.printStackTrace();
+        }
+    }
+
+    /*
+     * Kopiuje plik z resources do wskazanego miejsca, nadpisujac poprzedni
+     */
+    protected fun copyResource(name: String, file: File)
+    {
+        try
+        {
+            FileUtils.copyURLToFile(this.javaClass.classLoader.getResource(name), file)
+        }
+        catch (ex: IOException)
+        {
+            this.logger.error("Błąd podczas kopiowania zasobu '$name'!")
+            ex.printStackTrace()
+        }
     }
 }
