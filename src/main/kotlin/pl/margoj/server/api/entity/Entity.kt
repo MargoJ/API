7e58401515fef89ce9c0ba977c0cbc23014f1b47
KrekBuk <admin@margoj.pl>
2017-09-02 00:11:01 +0200

    Npc też mogą być martwe

--- src/main/kotlin/pl/margoj/server/api/entity/Entity.kt
+++ src/main/kotlin/pl/margoj/server/api/entity/Entity.kt
@@ -5,6 +5,7 @@ import pl.margoj.server.api.battle.Battle
 import pl.margoj.server.api.battle.BattleStats
 import pl.margoj.server.api.map.Location
 import pl.margoj.server.api.player.Gender
+import java.util.Date
 
 /**
  * Reprezentuje Entity (byt) w grze (npc, gracze)
@@ -70,6 +71,16 @@ interface Entity
      * Walka w jakiej znajduje sie entity
      */
     val currentBattle: Battle?
+
+    /**
+     * Data respawnu entity, może być nullem gdy entity jest żywe
+     */
+    val deadUntil: Date?
+
+    /**
+     * Czy entity jest martwy?
+     */
+    val isDead: Boolean
 
     /**
      * Zadaje [amount] obrażeń temu entity
